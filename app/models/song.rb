e06a0677e17ec468c46169a0f6617cdd512e3ff5
Kristin Ponsonby <kristinponsonby@gmail.com>
2021-07-04 08:55:39 -0400

    commit tree 5ff9d46465cc90fc58ccb21f066157e2eacf5547

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,13 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
-
+ 
   def artist_name
+    artist.name if artist
   end
 
   def artist_name=(name)
+    artist_name = Artist.find_or_create_by(name: name)
+    self.artist = artist_name
   end
+
 end
